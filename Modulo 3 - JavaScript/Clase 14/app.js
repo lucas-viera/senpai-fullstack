1755b06773d5d1f0cf91a34c948adc65b809362c
Lucas Viera <lucasviera22@gmail.com>
2022-02-02 21:50:43 -0300

    changed folder name

--- Modulo 3 - JavaScript/Clase 14/app.js
+++
@@ -1,8 +0,0 @@
-//Arrow functions
-
-const saludar = (nombre, apellido) => {
-    return `Hola! Mi nombre es ${nombre} ${apellido}`;
-}
-
-//Otra forma para cuando son de una sola linea
-const saludar2 = (nombre) => `Hola ${nombre}`;

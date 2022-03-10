54b3befca595508b7e1d45d6dc3e4db072e3c149
Lucas Viera <lucasviera22@gmail.com>
2022-03-09 23:55:21 -0300

    Update material clase 37 y Codigo clase 34

---
+++ Modulo 5 - NodeJS & Express/Clase 34 - Asynchronous/index.js
@@ -0,0 +1,9 @@
+const operation = require("./operations");
+
+const resultSuma = operation.sumar(2, 8);
+const resultResta = operation.restar(2, 8);
+const resultMult = operation.multiplicar(2, 8);
+
+console.log(resultSuma);
+console.log(resultResta);
+console.log(resultMult);
